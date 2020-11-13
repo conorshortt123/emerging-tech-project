f1602d677bc535e07f53857958b899bf2e1698f5
CONOR SHORTT <G00360253@gmit.ie>
2020-11-13 20:34:40 +0000

    Basic flask web application implemented with power prediction

---
+++ flask-web-application/api/power_predictor.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from flask import app
+import numpy as np
+from sklearn import tree
+import os
+import sys
+
+def predict_power(wind_speed):
+
+    print("PRINTING CURRENT DIRECTORY " + os.getcwd(), file=sys.stderr)
+
+    filename="./api/powerproduction.csv"
+
+    data = np.loadtxt(filename, delimiter=",", skiprows=1)
+
+    # Split numpy array into two arrays, a two dimensional array for x-values, and one dimensional for y-values.
+    x = data[:,[0]]
+    y = (data[:,[1]]).reshape(-1)
+
+    model = tree.DecisionTreeRegressor().fit(x, y)
+
+    pred = model.predict([[wind_speed]])
+
+    return np.asscalar(pred)
---
+++ flask-web-application/run.py
@@ -0,0 +1,27 @@
+# flask for web app.
+import flask as fl
+from flask import request
+# numpy for numerical work.
+import numpy as np
+# to navigate to api directory and retrieve power_predictor.py
+import sys
+sys.path.insert(1, './api')
+from power_predictor import predict_power
+
+# Create a new web app.
+app = fl.Flask(__name__)
+
+# Add root route.
+@app.route("/")
+def home():
+  return fl.render_template('index.html')
+
+# Add power predictor route.
+@app.route('/api/get-power-prediction')
+def power_prediction():
+
+  args = request.args
+
+  val = {"value": predict_power(args.get('windspeed'))}
+  
+  return val
