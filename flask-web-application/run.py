36a6b50feda76788d0273a24514ede853204ce4a
CONOR SHORTT <G00360253@gmit.ie>
2021-01-01 22:30:43 +0000

    Saved model from jupyter, now loads into Flask web app using Pickle, removed model training in seperate script for Flask

--- flask-web-application/run.py
+++ flask-web-application/run.py
@@ -3,25 +3,38 @@ import flask as fl
 from flask import request
 # numpy for numerical work.
 import numpy as np
-# to navigate to api directory and retrieve power_predictor.py
-import sys
-sys.path.insert(1, './api')
-from power_predictor import predict_power
+# To import decision_tree_model
+import pickle
 
 # Create a new web app.
 app = fl.Flask(__name__)
 
+
 # Add root route.
 @app.route("/")
 def home():
-  return fl.render_template('index.html')
+    return fl.render_template('index.html')
+
 
 # Add power predictor route.
 @app.route('/api/get-power-prediction')
 def power_prediction():
+    # Gets request arguments
+    args = request.args
 
-  args = request.args
+    # Creates a key : value pair containing the power prediction as a string with "kW/s" formatted on to it.
+    val = {"value": str(predict_power(args.get('windspeed'))) + " kW/s"}
 
-  val = {"value": str(predict_power(args.get('windspeed'))) + " kW/s"}
-  
-  return val
+    # Returns key : value pair.
+    return val
+
+
+def predict_power(wind_speed):
+    # Load model from disk with pickle.
+    filename = 'decision_tree_model/finalized_model.sav'
+    model = pickle.load(open(filename, 'rb'))
+
+    # Make prediction with model and return as a scalar value rather than an array.
+    pred = model.predict([[wind_speed]])
+
+    return np.asscalar(pred)
